76abdf648b8681ae70d112c7f68afd856e467b74
aph5nt <2.kamil.wojciechowski@gmail.com>
2025-11-23 18:41:50 +0100

    test refactor added ci

---
+++ tests/integration/pattern_detection/__init__.py
@@ -0,0 +1,6 @@
+"""
+Integration tests for pattern detection - Database storage validation.
+
+These tests require ClickHouse to be running.
+See tests/integration/README.md for setup instructions.
+"""
